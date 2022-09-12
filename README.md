307a49d919f56069aec670efbcc63aca738c3705
Kida <leslug.away@gmail.com>
2022-09-12 19:00:20 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Backend for Summoners
+
+## How to start
+
+1. Clone this repo
+2. npm restart
+3. Done
---
+++ index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { createServer } from 'http';
+import { Socket, Server } from 'socket.io';
+
+//create app
+const port = 8081;
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//connect app to websocket
+let http = createServer(app);
+let io = new Server(http);
+
+//websocket functions
+io.on('connection', (socket: Socket) => {
+    //sends heartbeat
+    socket.emit('Pong');
+});
+
+//api endpoints
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
