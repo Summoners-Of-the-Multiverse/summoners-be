51432ab2dcfc369dcee8fd85c2ca280f063cbfcd
Kida <leslug.away@gmail.com>
2022-09-13 02:16:42 +0800

    Added init, migrate, and rollback functions to npm run

---
+++ init.ts
@@ -0,0 +1,21 @@
+import DB from './src/DB';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(__dirname, '.env')});
+
+const DB_USER = process.env.DB_USER ?? "";
+const DB_PASSWORD = process.env.DB_PASSWORD ?? "";
+const DB_HOST = process.env.DB_HOST ?? "";
+const DB_PORT = process.env.DB_PORT ?? "5432";
+const DB_NAME = process.env.DB_NAME ?? "";
+
+let db = new DB({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: parseInt(DB_PORT),
+    database: DB_NAME,
+});
+
+db.migrate(true).then(() => console.log('DB Initialized Ended, press Ctrl + C to exit!'));
