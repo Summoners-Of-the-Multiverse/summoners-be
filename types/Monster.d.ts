589e6b6cdac9259e2b569ce28ce0aed4b2215ea3
Kida <leslug.away@gmail.com>
2022-09-14 17:29:04 +0800

    Updated types

--- types/Monster.d.ts
+++ types/Monster.d.ts
@@ -4,23 +4,38 @@ export type Monster = {
     token_id: string;
     attack: number;
     defense: number; 
+    hp: number;
     crit_chance: number;
-    stat_volatility: number;
+    crit_multiplier: number;
+    is_shiny: boolean;
 }
 
 export type MonsterBaseMetadata = {
     id: number;
     chain_id: string; 
+    element_type_id: number;
     name: string;
-    img_url: string;
+    img_file: string;
+    shiny_img_file: string;
+    shiny_chance: number;
+    base_attack: number;
+    max_attack: number;
+    base_defense: number;
+    max_defense: number;
+    base_hp: number;
+    max_hp: number;
+    base_crit_chance: number;
+    max_crit_chance: number;
+    base_crit_multiplier: number;
+    max_crit_multiplier: number;
 }
 
 export type MonsterSkill = {
     id: number;
-    monster_base_metadata_id: number;
-    type_id: number;
+    element_type_id: number;
     effect_id: number;
     name: string;
+    hits: number;
     accuracy: number;
     cooldown: number;
     multiplier: number;
@@ -28,14 +43,13 @@ export type MonsterSkill = {
 
 export type MonsterSkillEffect = {
     id: number;
-    name: string;
-    effect_asset_url: string;
+    asset_file: string;
 }
 
-export type MonsterSkillEffectType = {
+export type Element = {
     id: number;
-    name: string;
-    icon_url: string;
+    name: number;
+    icon_file: number;
 }
 
 export type ElementMultipler = {
@@ -52,6 +66,20 @@ export type Area = {
 
 export type AreaMonster = {
     id: number;
-    monster_id: number;
+    monster_base_metadata_id: number;
+    area_id: number;
     stat_modifier: number;
+}
+
+// in roster
+export type PlayerMonster = {
+    id: number;
+    address: string;
+    monster_id: number;
+}
+
+export type MonsterEquippedSkill = {
+    id: number;
+    monster_id: number;
+    monster_skill_id: number;
 }
